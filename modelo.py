e04025fff79e2564dbd60430d5e2e1894505d94f
Julio Facal <starlyn.dev@gmail.com>
2022-10-16 10:00:00 -0300

    adicionando atributos, metodos e impressao formatada

--- modelo.py
+++ modelo.py
@@ -1,15 +1,27 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+        
+    def like(self):
+        self.likes += 1
         
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
         
-vingadores = Filme()
+    def like(self):
+        self.likes += 1
+        
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 
-atlanta = Serie()
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+
+atlanta = Serie('atlanta', 2018, 2)
+
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
